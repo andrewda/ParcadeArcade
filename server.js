ec3814c54b77766a0761fbd0acbd8e6fce60eb12
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-02-12 23:49:59 -0800

    Add point management

--- server.js
+++ server.js
@@ -1,17 +1,84 @@
 var express = require('express');
 var app = express();
-var handlebars = require('handlebars');
 var fs = require('fs');
+var MongoClient = require('mongodb');
+var md5 = require('md5');
 var port = process.env.PORT || 1337;
 
-var layout = handlebars.compile(fs.readFileSync('./layout.html', 'utf8'));
+MongoClient.connect('mongodb://admin:4dm1n_u53r@ds061385.mongolab.com:61385/parcade-arcade', function(err, db) {
 
-app.get('/', function (req, res) {
-    res.end(layout({
-        "data": "Hello world!"
-    }));
-});
+    /**
+     * On a get request to /, check the number of points the provided user
+     * currently has. This should later be moved to /get_points or similar.
+     */
+    app.get('/', function(req, res) {
+        var query = req.query;
+        var cursor = db.collection('users').find({
+            'id': query.id
+        });
 
-app.listen(port, function () {
-    console.log('Listening at port', port);
+        res.setHeader('Content-Type', 'application/json');
+
+        cursor.each(function(err, doc) {
+            if (doc !== null) {
+                res.end( JSON.stringify( { success: true, id: query.id, points: doc.points } ) );
+            } else {
+                res.end( JSON.stringify( { success: false, error: 'no document with id ' + query.id } ) );
+            }
+        });
+    });
+
+    /**
+     * On a POST request to /points, add points to the user with a specific
+     * userId.
+     */
+    app.post('/points', function(req, res) {
+        var query = req.query;
+        console.log(query)
+        var cursor = db.collection('users').find({
+            'id': query.id
+        });
+
+        res.setHeader('Content-Type', 'application/json');
+
+        cursor.each(function(err, doc) {
+            if (doc !== null) {
+                console.log(doc);
+
+                db.collection('users').update({
+                    'id': query.id
+                }, {
+                    'id': query.id,
+                    'points': parseInt(doc.points) + parseInt(query.points)
+                });
+
+                res.end( JSON.stringify( { success: true, points: parseInt(doc.points) + parseInt(query.points) } ) );
+            } else {
+                res.end( JSON.stringify( { success: false, error: 'no document with id ' + query.id } ) );
+            }
+        });
+    });
+
+    /**
+     * On a POST request to /create_user, add a user to our MongoDB server
+     * using the md5 hash of the user's remote IP address as their userId.
+     */
+    app.post('/create_user', function(req, res) {
+        var userId = md5(req.connection.remoteAddress);
+
+        db.collection('users').insert({
+            'id': userId,
+            'points': 0
+        }, function(err) {
+            if (err) {
+                res.end( JSON.stringify( { success: false, error: err } ) );
+            } else {
+                res.end( JSON.stringify( { success: true, id: userId } ) );
+            }
+        });
+    });
+
+    app.listen(port, function() {
+        console.log('Listening at port', port);
+    });
 });
