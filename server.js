59cf9c8d30ecd500d9d0a823aef04a4a06c22e13
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-02-12 20:44:01 -0800

    Use handlebars

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var handlebars = require('handlebars');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
 
+var layout = handlebars.compile(fs.readFileSync('./layout.html', 'utf8'));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.end(layout({
+        "data": "Hello world!"
+    }));
 });
 
 app.listen(port, function () {
