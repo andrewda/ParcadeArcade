614781211712052b0c5534ba5cd9e12fb6a0bf78
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-02-12 19:54:54 -0800

    Use Express

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var http = require('http')
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello ParcadeArcade\n');
-}).listen(port);
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(port, function () {
+    console.log('Listening at port', port);
+});
